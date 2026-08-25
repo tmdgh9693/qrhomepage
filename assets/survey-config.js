65e59b0656ecccbc542f7ade696cfac7ba47eae7
tmdgh9693 <tmdgh9693@gmail.com>
2026-08-25 15:24:14 +0900

    Delete assets directory

--- assets/survey-config.js
+++
@@ -1,9 +0,0 @@
-window.SURVEY_CONFIG = Object.freeze({
-  provider: 'supabase',
-  supabaseUrl: 'https://rkxqayycirimuptduwwy.supabase.co',
-  supabasePublishableKey: 'sb_publishable_dFlJrnyhyXKMcdwEmjLKQA_D8gGDa0Z',
-  tableName: 'survey_responses',
-  surveyVersion: '2026-01',
-  duplicateStorageKey: 'yeosuLighthouseSurveyCompleted_2026_01',
-  allowLocalPreview: true
-});
